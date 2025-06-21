7fc2d794b70826b3e92407b0d570dc338c7cae4f
ADMIN <alvinalioya@gmail.com>
2025-06-21 23:54:47 +0300

    Initial clean push

--- photostudio/settings.py
+++ photostudio/settings.py
@@ -33,6 +33,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
